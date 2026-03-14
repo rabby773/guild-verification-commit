2563cd2ad980b0cf9de58786c542463e03caf218
rabby773 <rabby773islam@gmail.com>
2026-03-14 06:51:00 +0600

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T00:50:53.996Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-14T00:51:16.877Z
+Commit: Add multi-language support
